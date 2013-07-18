92028d572de362a9e5da5cd3c2eadcb27677cc65
ChevalierP <chevalierp01@gmail.com>
2013-07-18 15:13:38 +0200

    Update main.c

--- main.c
+++ main.c
@@ -5,10 +5,11 @@ void conversion(int* h, int* m);
 int main()
 {
     int minute=0, heure=0;
+    int* pointeurMinute=&minute;
     printf("Indiquer un nombre de minutes :");
-    scanf("%d", minute);
+    scanf("%p", pointeurMinute);
     conversion(&heure, &minute);
-    printf("\n Ce qui correspond a %d heure(s) et %d miute(s)", heure, minute);
+    printf("\n Ce qui correspond a %d heure(s) et %d minute(s)", heure, minute);
 
     return 0;
 }
