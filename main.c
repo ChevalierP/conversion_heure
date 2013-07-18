dba621a9689b7147f128d68be3a0e50d45503484
ChevalierP <chevalierp01@gmail.com>
2013-07-18 15:31:28 +0200

    Update main.c
    ajout d'une boucle

--- main.c
+++ main.c
@@ -4,12 +4,17 @@
 void conversion(int* h, int* m);
 int main()
 {
+    int continuer=0;
+    do
+    {
     int minute=0, heure=0;
     printf("Indiquer un nombre de minutes :");
-    scanf("%p", &Minute);
+    scanf("%p", &minute);
     conversion(&heure, &minute);
-    printf("\n Ce qui correspond a %d heure(s) et %d minute(s)", heure, minute);
-
+    printf("\n Ce qui correspond a %d heure(s) et %d minute(s)\n", heure, minute);
+    printf("Une autre convertion ? Oui(1)/Non(0)");
+    scanf("%d",&continuer);
+    }while(continuer!=0);
     return 0;
 }
 void conversion(int* h, int* m)
