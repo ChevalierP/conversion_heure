e47ffebb92666ec3c559920eea8986cb8133fe1e
ChevalierP <chevalierp01@gmail.com>
2013-07-20 10:50:21 +0200

    Update main.c
    correction bug de conversion

--- main.c
+++ main.c
@@ -9,10 +9,10 @@ int main()
     {
     int minute=0, heure=0;
     printf("Indiquer un nombre de minutes :");
-    scanf("%p", &minute);
+    scanf("%d", &minute);
     conversion(&heure, &minute);
     printf("\n Ce qui correspond a %d heure(s) et %d minute(s)\n", heure, minute);
-    printf("Une autre convertion ? Oui(1)/Non(0)");
+    printf("Une autre conversion ? Oui(1)/Non(0)");
     scanf("%d",&continuer);
     }while(continuer!=0);
     return 0;
